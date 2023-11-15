6adcd10856b9e31ca059090b830d6c46d9d1e3de
abc1123 <465970168@qq.com>
2023-11-15 23:32:39 +0900

    cookie

--- src/test.py
+++ src/test.py
@@ -1,4 +1,10 @@
 import requests
+from pyquery import PyQuery as pq
 
 response = requests.get(url='https://koto-hsc3.revn.jp/auth/login')
-print(response.status_code)
+doc = pq(response.content.decode('utf-8'))
+_csrfToken = doc('input[name=_csrfToken]')
+#print(_csrfToken)
+
+cookie = response.cookies
+print(cookie.get('csrfToken'))
